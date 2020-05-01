bbaf8ee2677de5b47fa7b6c6b8abe8063483d36e
Ben Moussa Chaker <chaker@MacBook-Pro-de-Chaker.local>
2020-05-01 19:15:04 +0200

    Updates

---
+++ precache-manifest.c6ca13d353361c506f53c86d594ea09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b7d7d2aed148cb9f97087969ac6701",
+    "url": "/compteur/index.html"
+  },
+  {
+    "revision": "5bd048ea79e6c9493e1e",
+    "url": "/compteur/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "c9854496390bce1628b7",
+    "url": "/compteur/static/js/2.fb92a806.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/compteur/static/js/2.fb92a806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd048ea79e6c9493e1e",
+    "url": "/compteur/static/js/main.25d0dace.chunk.js"
+  },
+  {
+    "revision": "9cb88709f4d7ae634710",
+    "url": "/compteur/static/js/runtime-main.b4346028.js"
+  }
+]);
