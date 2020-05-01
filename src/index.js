2b03da66bc46e4551224856e683b3db40deeffbc
Ben Moussa Chaker <chaker@MacBook-Pro-de-Chaker.local>
2020-05-01 19:08:42 +0200

    init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux'
+import reducer from './Store/reducer'
+import {Provider} from 'react-redux'
+const store  = createStore(reducer)
+ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
    
     
 
